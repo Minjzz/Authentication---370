9bc4b083fab710f90bec2a4ca843333218d5afbb
Marlon Villegas <marlonvillegas00@gmail.com>
2026-02-21 10:17:25 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include 'config.php';
+
+if(isset($_POST['login'])){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username=?");
+    $stmt->bind_param("s",$username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows > 0){
+        $user = $result->fetch_assoc();
+
+        if(password_verify($password, $user['password'])){
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['role'] = $user['role'];
+
+            header("Location: dashboard.php");
+            exit();
+        } else {
+            echo "Wrong password!";
+        }
+    } else {
+        echo "User not found!";
+    }
+}
+?>
+
+<form method="POST">
+<h2>Login</h2>
+<input type="text" name="username" placeholder="Username" required><br><br>
+<input type="password" name="password" placeholder="Password" required><br><br>
+<button name="login">Login</button>
+</form>
